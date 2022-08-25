73f7344c65c79c4e7770c5f291450408b3ca9e2a
Gabrielle Stein <gabbystein125@gmail.com>
2022-08-25 19:20:00 -0400

    Add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Gabrielle Stein Portfolio</title>
         <meta name="description" content="Gabrielle Stein Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -21,8 +21,37 @@ const Home: NextPage = () => {
           Software Engineer
         </p>
 
-        <div className={styles.grid}>
-        
+        <div className={styles.column}>
+          Current Projects:
+          <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h2>Documentation &rarr;</h2>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h2>Learn &rarr;</h2>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/canary/examples"
+            className={styles.card}
+          >
+            <h2>Examples &rarr;</h2>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h2>Deploy &rarr;</h2>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+          </div>
         </div>
       </main>
 
